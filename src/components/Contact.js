6cb49d6e0ac6373828e1cd8985dffc32578467ce
Kon <konsonoftek@gmail.com>
2021-01-23 23:49:38 -0500

    Fixing email system

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -64,7 +64,7 @@ const Form = styled.form`
 const Contact = () => {
   const [data, setData] = useState({ name: '', email: '', subject:'', message: ''});
   const userID = process.env.REACT_APP_USER_ID;
-  
+
   const handleChange = (e) => {
     const { name, value } = e.target
     setData({
@@ -90,7 +90,7 @@ const Contact = () => {
       subject: data.subject,
       message: data.message
     }
-      emailjs.send('gmail', 'template_f6onltg', templateParams, userID)
+      emailjs.send('gmail', 'template_f6onltg', templateParams, 'user_C8s5kqzohKIEtX9F9viGX')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
