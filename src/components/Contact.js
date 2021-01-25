acc09e8d9206d6091454aa188e1ab25a399acf4b
Kon <konsonoftek@gmail.com>
2021-01-25 15:05:13 -0500

    added env var

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -63,7 +63,7 @@ const Form = styled.form`
 
 const Contact = () => {
   const [data, setData] = useState({ name: '', email: '', subject:'', message: ''});
-  const userID = "user_C8s5kqzohKIEtX9F9viGX";
+  const userID = process.env.REACT_APP_USER;
   
   const handleChange = (e) => {
     const { name, value } = e.target
